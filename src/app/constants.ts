6006aecfc0cf721c7a6a1f87b585e3b3dfd4e294
Perry George <pgeorge@adaptavist.com>
2025-01-15 16:40:47 +0000

    refactor: updated constants api url

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -5,7 +5,7 @@ if(!process.env.NEXT_PUBLIC_SALABLE_PRO_PLAN_UUID) throw new Error('Missing env 
 if(!process.env.NEXT_PUBLIC_PRODUCT_UUID) throw new Error('Missing env NEXT_PUBLIC_PRODUCT_UUID')
 if(!process.env.NEXT_PUBLIC_APP_BASE_URL) throw new Error('Missing env NEXT_PUBLIC_APP_BASE_URL')
 
-export const salableApiBaseUrl = 'https://api/salable.app'
+export const salableApiBaseUrl = 'https://api.salable.app'
 export const salableBasicPlanUuid = process.env.NEXT_PUBLIC_SALABLE_PLAN_UUID
 export const salableProPlanUuid = process.env.NEXT_PUBLIC_SALABLE_PRO_PLAN_UUID
 export const salableProductUuid = process.env.NEXT_PUBLIC_PRODUCT_UUID
