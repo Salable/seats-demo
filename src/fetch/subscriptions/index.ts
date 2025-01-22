47b9b035be84686a18da1cd2d477f37d10b68291
Perry George <pgeorge@adaptavist.com>
2025-01-22 15:55:38 +0000

    refactor: added sort and planUuid to get all subscriptions

--- src/fetch/subscriptions/index.ts
+++ src/fetch/subscriptions/index.ts
@@ -6,6 +6,7 @@ import { Result } from "@/app/actions/checkout-link";
 import {salable} from "@/app/salable";
 import {PaginatedSubscriptionInvoice, Subscription, Plan, PlanCurrency} from "@salable/node-sdk/dist/src/types";
 import {SalableResponseError} from "@salable/node-sdk";
+import {salableProductUuid} from "@/app/constants";
 
 export type SubscriptionExpandedPlan = Subscription & {
   plan: Plan
@@ -29,8 +30,8 @@ export async function getAllSubscriptions(): Promise<Result<GetAllSubscriptionsE
     const data = await salable.subscriptions.getAll({
       email: session.email,
       expand: ['plan'],
-      // sort: 'desc',
-      // productUuid: salableProductUuid
+      sort: 'desc',
+      productUuid: salableProductUuid
     }) as GetAllSubscriptionsExpandedPlan
     return {
       data, error: null
